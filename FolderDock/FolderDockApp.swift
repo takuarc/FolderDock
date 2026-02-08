19da1c6a7121d033b832db9550d52c73e79dfc16
takuarc <awychoi@gmail.com>
2026-02-07 21:58:06 -0500

    Initial Commit

---
+++ FolderDock/FolderDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderDockApp.swift
+//  FolderDock
+//
+//  Created by awychoi on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FolderDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
